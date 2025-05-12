7aecacf1dd58df6d893d43d6eb401b1b2c7242be
Jonathan Búcaro <44926768+B-caro@users.noreply.github.com>
2025-05-12 15:40:39 -0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "➡️  Creando archivo de usuarios..."
+echo "admin:{SHA256}8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918" > /data/users.txt
+
+echo "➡️  Creando archivo de configuración de resolver..."
+echo '{ "type": "passwdresolver", "filename": "/data/users.txt" }' > /data/resolver.conf
+
+echo "➡️  Creando resolver..."
+pi-manage resolver create adminresolver passwdresolver /data/resolver.conf
+
+echo "➡️  Creando realm..."
+pi-manage realm create adminrealm adminresolver
+
+echo "➡️  Creando política de login para WebUI..."
+pi-manage policy create loginpolicy webui login adminrealm admin
+
+echo "✅ Configuración inicial completada."
